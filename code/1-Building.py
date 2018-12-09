eb2afeca0bcd49bca8910bbf8b4ef9a46d8eece8
huxd-laptop <mydonne@gmail.com>
2018-12-09 10:54:13 +0800

    init
    first commit

---
+++ code/1-Building.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  8 10:33:33 2018
+
+@author: HUXD
+"""
+
+import os
+
+programdir = "E:\\TESTDATA\\TestProgram-BuildingExtract-1121\\DinkNet\\"
+
+retcode = os.system(programdir + "predict.exe")
